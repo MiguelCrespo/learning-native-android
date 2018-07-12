742d23194b4b7b3b683fd41315db9aebbbb504db
Miguel Crespo <miguel.crespo6@gmail.com>
2018-07-11 19:25:56 -0500

    Building some interfaces

--- githubViewer/app/src/main/java/com/example/miguelcrespo/githubviewer/UserDetails.kt
+++ githubViewer/app/src/main/java/com/example/miguelcrespo/githubviewer/UserDetails.kt
@@ -1,8 +1,15 @@
 package com.example.miguelcrespo.githubviewer
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.BitmapImageViewTarget
 import kotlinx.android.synthetic.main.activity_user_details.*
 
 class UserDetails : AppCompatActivity() {
@@ -17,7 +24,30 @@ class UserDetails : AppCompatActivity() {
 
         println(mUser.toString())
 
+        /*val requestOption = RequestOptions()
+                .centerCrop()
+                .transforms(CenterCrop(), RoundedCorners(500))
 
-        Glide.with(this).load(mUser.avatar_url).into(image_profile)
+        Glide.with(this).load(mUser.avatar_url)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .apply(requestOption)
+                .into(image_profile)*/
+
+        Glide.with(this).load(mUser.avatar_url)
+                .into(image_profile)
+
+        button_view_profile.setOnClickListener{v ->
+            openUserProfile(v)
+        }
+
+
+    }
+
+    fun openUserProfile(v: View) {
+        val intent = Intent(this@UserDetails, UserProfile::class.java)
+
+        intent.putExtra(MainActivity.USER, mUser)
+
+        startActivity(intent)
     }
 }
